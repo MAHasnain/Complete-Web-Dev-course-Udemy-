744c468e7065c83809c443a4f14469df29840e9a
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-10-02 12:27:55 +0500

    Investigative study of grid almost done.

---
+++ JAVASCRIPT_section/JS_Foundation/primitive_datatypes.js
@@ -0,0 +1,14 @@
+// Number
+
+let balance = 34;
+const anotherBalance = new Number(balance);
+
+console.log(typeof anotherBalance);
+
+
+
+
+const symbol1 = new Symbol();
+const symbol2 = new Symbol();
+
+console.log(symbol1 == symbol2)
