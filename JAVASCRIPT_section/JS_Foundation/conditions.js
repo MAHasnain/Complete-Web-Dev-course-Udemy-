0c9b4ab944e40e98099464feeceea369c322f619
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-10-03 21:15:51 +0500

    conditions investigative study

---
+++ JAVASCRIPT_section/JS_Foundation/conditions.js
@@ -0,0 +1,34 @@
+// Write a function that takes a number as input and returns whether the number is "even" or "odd".
+
+// Write a function that checks if a number is positive, negative, or zero and logs the result.
+
+// Create a program that checks if a given year is a leap year. Return true for leap years and false otherwise.
+
+// Write a function that takes two numbers and returns the largest one. If the numbers are equal, return "Both numbers are equal."
+
+// Create a function that accepts three numbers and checks if a triangle can be formed with them. Use the triangle inequality theorem to determine this.
+
+let isTeaReady = true;
+if (isTeaReady) {
+  console.log(`Tea is ready.`);
+} else {
+  console.log(`Tea is not ready.`);
+}
+
+let num1 = 3;
+let num2 = 7;
+
+// if (num1 > num2) {
+//   console.log(`first number is greater than second number.`);
+// } else {
+//   console.log(`second number is greater than first number.`);
+// }
+
+let num = 65;
+if (num > 0) {
+  console.log(`your given number is greater than zero.`);
+} else if (num < 0) {
+  console.log(`Your given number is negative.`);
+} else {
+  console.log(`Your given number is zero.`);
+}
