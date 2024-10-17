4f54ad18429cc7b2465180ae958dfeaa58a07076
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-10-17 17:22:18 +0500

    getter and setter remains

--- JAVASCRIPT_section/JS_OOPS/pillarsOfOOP.js
+++ JAVASCRIPT_section/JS_OOPS/pillarsOfOOP.js
@@ -16,4 +16,59 @@ class BankAccount {
 
 const account = new BankAccount();
 // account.deposit(2e3)
-console.log(account.getBalance())
+// console.log(account.getBalance())
+
+
+// Abstraction 
+
+class CoffeeMachine {
+  
+  start() {
+
+    // DB calls
+    return `starting machine ...`
+  }
+
+  brewCoffee (){
+    return `Brewing Coffee`
+  }
+
+  pressStartButton(){
+    // return `${this.start()} and ${this.brewCoffee()}`
+    
+    let msgOne = this.start()
+    let msgTwo = this.brewCoffee()
+
+    return `${msgOne} and ${msgTwo}`
+  }
+}
+
+const oldMachine = new CoffeeMachine();
+// console.log(oldMachine.pressStartButton())
+
+
+
+// Polymorphism
+
+class Bird {
+  fly(){
+    return `flying ...`
+  }
+
+}
+
+class Penguin extends Bird {
+
+  fly() {
+    return `Penguins can't fly`
+  }
+}
+
+const sparrow = new Bird();
+console.log(`sparrow ${sparrow.fly()}`)
+
+const penguin = new Penguin();
+
+
+
+
