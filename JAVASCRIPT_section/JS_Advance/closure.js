e578df9b515a414d42b771196a18413fd3fd6926
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-02 11:30:11 +0500

    closure learnt

---
+++ JAVASCRIPT_section/JS_Advance/closure.js
@@ -0,0 +1,24 @@
+// function outer() {
+//     let counter = 4
+//     return function () {
+//         counter ++;
+//         return counter;
+//     }
+// }
+// let increment = outer()
+
+// console.log(increment())
+// console.log(increment())
+
+
+// function outer() {
+//     let counter = 5;
+//     return function () {
+//         counter ++
+//         return counter
+//     }
+// }
+
+// let increment = outer()
+// console.log(increment())
+
