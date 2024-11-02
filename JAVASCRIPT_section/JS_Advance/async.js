23451e73d781fa2cb655276b8c69fe126d4321de
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-02 11:29:14 +0500

    investigative study of asynchronous javascript

---
+++ JAVASCRIPT_section/JS_Advance/async.js
@@ -0,0 +1,17 @@
+
+function greeting() {
+    console.log("I would say like to say Hello")
+}
+
+
+// greeting()
+
+setTimeout(() => {  /// third
+    greeting()
+}, 3000);
+
+console.log("Hello MAH");  /// this first 
+
+for (let i = 0; i < 10; i++) {  /// 2nd
+    console.log(i)
+}
