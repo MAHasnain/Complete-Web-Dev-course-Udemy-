a4a2997263bd3738a0b5569b526b253d072bd68d
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-03 11:46:13 +0500

    ES6 modules and Common JS learnt by Exporting functions

---
+++ JAVASCRIPT_section/JS_Advance/appC.js
@@ -0,0 +1,8 @@
+const mathOperations = require('./mathOperationC.js');
+
+console.log(mathOperations.add(20, 8))
+
+console.log(mathOperations.subtract(20, 6))
+
+
+console.log(mathOperations.multiply(2, 7))
---
+++ JAVASCRIPT_section/JS_Advance/appM.mjs
@@ -0,0 +1,12 @@
+import { add, multiply } from "./mathOperationsM.mjs";  
+
+let addition = add(3, 6);
+console.log(addition);
+
+let multiplication = multiply(5, 7);
+console.log(multiplication);
+
+import subtract from "./mathOperationsM.mjs";   //// default
+
+// console.log(subtract(2, 5));
+console.log(subtract(8, 5));
