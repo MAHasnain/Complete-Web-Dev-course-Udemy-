788c0319d28d0830551e948839187819853e95e6
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-04 11:19:12 +0500

    almost done

--- JAVASCRIPT_section/JS_Advance/Hands_On_JS_advance_Challenges/closres.js
+++ JAVASCRIPT_section/JS_Advance/Hands_On_JS_advance_Challenges/closres.js
@@ -4,10 +4,10 @@
 // Create a function createCounter() that returns a function which increments and returns a counter value each time it is called.
 
 function createCounter() {
-    let counter = 4
-    return function() {
-        return counter ++
-    }
+  let counter = 4;
+  return function () {
+    return counter++;
+  };
 }
 let increments = createCounter();
 // console.log(increments())
@@ -18,10 +18,40 @@ let increments = createCounter();
 
 // Create a function rateLimiter(fn, limit) that returns a new function. The returned function allows calling fn only once within a limit time in milliseconds. If it is called again before the limit is reached, it should return "Rate limit exceeded".
 
-function rateLimiter() {
-    
+function rateLimiter(fn, limit) {
+  let lastCallTime = 0;
+  return function (...arg) {
+    const now = Date.now();
+
+    if (now - lastCallTime < limit) {
+        return "Rate limit exceeded."
+    } else {
+        lastCallTime = now;
+        return fn(...arg)
+    }
+  };
 }
+
+function sayHello(name) {
+    return `Hello ${name}`
+}
+
+const limitedSayHello = rateLimiter(sayHello, 3000);
+
+// console.log(limitedSayHello("alice"))
+// console.log(limitedSayHello("Bob"))
+
+// setTimeout(() => console.log(limitedSayHello("Bob")), 3000);
+
+// Explanation:
+// The rateLimiter function takes a function fn and a limit in milliseconds as parameters.
+// It keeps track of the lastCallTime to know when fn was last called.
+// When the returned function is invoked, it checks if the current time minus lastCallTime is less than the limit.
+// If it is, it returns "Rate limit exceeded".
+// Otherwise, it updates lastCallTime to the current time and calls fn with the given arguments.
+
 
 // Task 3: Memoization Function
 
 // Write a function memoize(fn) that returns a memoized version of fn. The memoized function should cache the results of function calls, and return the cached result if the same inputs are provided again.
+
