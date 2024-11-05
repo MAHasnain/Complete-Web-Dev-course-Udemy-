461823b74a12dfc50b81c97342daa20125b525eb
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-05 10:40:38 +0500

    tasks almost complete

--- JAVASCRIPT_section/JS_Advance/Hands_On_JS_advance_Challenges/Async-Await_and_Promise.all.js
+++ JAVASCRIPT_section/JS_Advance/Hands_On_JS_advance_Challenges/Async-Await_and_Promise.all.js
@@ -2,10 +2,39 @@
 // Task 1: Async-Await with Promise.all
 
 // Create two functions fetchUser() and fetchPosts(), both returning promises that resolve in 1 second.
-
 // Use async-await and Promise.all to fetch both simultaneously and log the results as part of fetchAllData()
 
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`User fetched.`);
+    }, 1000);
+  });
+}
 
+function fetchPosts() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Posts Fetched.`);
+    }, 1000);
+  });
+}
+
+async function fetchAllData() {
+  try {
+    console.log(`fetching...`);
+    const [userData, postsData] = await Promise.all([
+      fetchUser(),
+      fetchPosts(),
+    ]);
+
+    console.log(userData);
+    console.log(postsData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// fetchAllData()
 
 // Task 2: Error Handling in Async/Await with Promise.all
 
@@ -13,8 +42,56 @@
 
 // Create a function handlePromises() that calls both functions using Promise.all and handles success and failure cases.
 
+function fetchSuccess() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`fetching succeed.`);
+    }, 1000);
+  });
+}
 
+function fetchFailure() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(`fetching failed.`);
+    }, 1000);
+  });
+}
+
+async function handlePromises() {
+  try {
+    const [succeed, failed] = await Promise.all([
+      fetchSuccess(),
+      fetchFailure(),
+    ]);
+
+    console.log(succeed);
+    console.log(failed);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// handlePromises();
 
 // Task 3: Timeout with Async/Await and Promise.race
 
 // Create a function fetchWithTimeout(promise, timeout) that takes a promise and a timeout value in milliseconds. Use Promise.race() to return the result of the promise if it resolves within the timeout, otherwise return "Timeout exceeded".
+
+function fetchWithTimeout(promise, timeout) {
+  const promiseTimeOut = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(`Timeout exceeded.`);
+    }, timeout);
+  });
+  return Promise.race([promise, promiseTimeOut]);
+}
+
+const examplePromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(`Data received successfully.`);
+  }, 1000);
+});
+
+fetchWithTimeout(examplePromise, 500)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
