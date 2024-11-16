4f537cbc5d3c33f7585dff5f7d79eafe5aabd2ba
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-16 17:48:52 +0500

    some tasks remains

--- JS_Projects_section/Project_5_Quiz_app/script.js
+++ JS_Projects_section/Project_5_Quiz_app/script.js
@@ -1,5 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const quizContainer = document.getElementById("quiz-container");
+  const questionText = document.querySelector(".question-text");
+  const choicesList = document.getElementById("choices-list");
+  const resultContainer = document.getElementById("result-container");
+  const scoreDisplay = document.getElementById("score");
+  const nextBtn = document.getElementById("next-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const startBtn = document.getElementById("start-btn");
 
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      choices: ["Mars", "Venus", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "Who wrote 'Hamlet'?",
+      choices: [
+        "Charles Dickens",
+        "Jane Austen",
+        "William shakespeare",
+        "Mark Twain",
+      ],
+    },
+  ];
 
-    
+  let currentQuestionIndex = 0;
+  let score = 0;
+
+  startBtn.addEventListener("click", startQuiz);
+
+  function startQuiz() {
+    startBtn.classList.add("hidden");
+    resultContainer.classList.add("hidden");
+    quizContainer.classList.remove("hidden");
+    showQuestion();
+  }
+
+  function showQuestion() {
+    nextBtn.classList.add("hidden");
+    questionText.textContent = questions[currentQuestionIndex].question;
+    choicesList.innerHTML = "";
+    questions[currentQuestionIndex].choices.forEach((choice) => {
+      const li = document.createElement("li");
+      li.textContent = choice;
+      li.addEventListener("click", () => selectAnswer(choice));
+      choicesList.appendChild(li);
+    });
+  }
+
+  function selectAnswer(choice) {
+    const correctAns = questions[currentQuestionIndex].answer;
+    if (correctAns === choice) {
+      score++;
+    }
+    nextBtn.classList.add('hidden');
+  }
+
 });
