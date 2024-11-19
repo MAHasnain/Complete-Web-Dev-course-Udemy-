3d20c97c31e0f8372f00f0971fc0027c08d18c7a
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-19 12:09:08 +0500

    Nginx server with NodeJS

---
+++ Backend_section/nginx_server_with_nodejs/server.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const http = require("http");
+const fs = require("fs");
+
+const port = 5500;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+  
+  const extName = String(path.extname(filePath).toLowerCase());
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: file not found Bro");
+      }
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
