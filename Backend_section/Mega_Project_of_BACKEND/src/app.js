4c4dadc86532ed4431b11e256357403c01d672ae
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-25 01:19:11 +0500

    creation of healthcheck route and testing with postman

--- Backend_section/Mega_Project_of_BACKEND/src/app.js
+++ Backend_section/Mega_Project_of_BACKEND/src/app.js
@@ -1,18 +1,44 @@
 import express from "express";
 import cors from "cors";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 
 // common middleware
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
+const morganFormat = ":method :url :status :response-time ms";
 
+// Logger
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+// import routes
+import router from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", router);
 
 export { app };
---
+++ Backend_section/Mega_Project_of_BACKEND/src/routes/healthCheck.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { healthcheck } from "../controller/healthcheck.controllers.js";
+
+const router = Router();
+
+router.route("/").get(healthcheck);
+
+export default router;
