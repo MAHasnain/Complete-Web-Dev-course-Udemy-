cfd363424fd2585254dfb5447e8689d81163ddf2
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-24 11:18:06 +0500

    whole project structured and database connected

---
+++ Backend_section/Mega_Project_of_BACKEND/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstances = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected ! DB host: ${connectionInstances.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MongoDB connection error`, error);
+    process.exit(1);
+  }
+};
+export default connectDB;
---
+++ Backend_section/Mega_Project_of_BACKEND/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 5501;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connetion error: ${err}`);
+  });
