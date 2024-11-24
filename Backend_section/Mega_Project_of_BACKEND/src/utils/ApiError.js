58df975969bbb89d24f7bff5d160f0812563a248
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-24 12:12:48 +0500

    standardised error

---
+++ Backend_section/Mega_Project_of_BACKEND/src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    error = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.error = error;
+  }
+}
+
+export { ApiError };
