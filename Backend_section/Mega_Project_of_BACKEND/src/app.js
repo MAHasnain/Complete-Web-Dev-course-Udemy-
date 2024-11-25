bf4033ce66611af06a27c0ef953c332a23a59b9d
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2024-11-26 01:21:07 +0500

    some more packages (jwt, bcrypt, multer, cloudinary) installed in my project

--- Backend_section/Mega_Project_of_BACKEND/src/app.js
+++ Backend_section/Mega_Project_of_BACKEND/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import logger from "./logger.js";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
--- Backend_section/Mega_Project_of_BACKEND/src/models/user.models.js
+++ Backend_section/Mega_Project_of_BACKEND/src/models/user.models.js
@@ -13,6 +13,8 @@ updatedAt Date
  */
 
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -61,5 +63,59 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
+
+/**
+ * Generates an access token for the user.
+ * The token contains the user's ID, name, email, and username.
+ * It is signed using the ACCESS_TOKEN_SECRET environment variable.
+ * The token's expiration time is specified by the ACCESS_TOKEN_EXPIRY environment variable.
+ */
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+
+/**
+ * Generates a refresh token for the user.
+ * The token contains the user's ID.
+ * It is signed using the REFRESH_TOKEN_SECRET environment variable.
+ * The token's expiration time is specified by the REFRESH_TOKEN_EXPIRY environment variable.
+ */
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
