8874ba324316aecb18771f778a40406818b179e9
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-12-19 15:57:03 +0500

    React first hello world project created

---
+++ Frontend_section/01_react_basics/src/App.jsx
@@ -0,0 +1,14 @@
+import Greeting from "./components/greeting.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <Greeting />
+        {/* Hello MAH */}
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend_section/01_react_basics/src/components/greeting.jsx
@@ -0,0 +1,5 @@
+const Greeting = () => {
+  return <h1>Hello MAH</h1>;
+};
+
+export default Greeting;
