ecb2ac9801cecee094088c3d67ead9dee4b67e49
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-01-04 11:13:23 +0500

    Add Redux toolkit integration for authentication and implement Header/Footer components

--- Frontend_section/09_Mega-Project_Blog-App/src/App.jsx
+++ Frontend_section/09_Mega-Project_Blog-App/src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <>
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
+  ) : null;
+};
+
+export default App;
---
+++ Frontend_section/09_Mega-Project_Blog-App/src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from "./Footer/Footer.jsx";
+import Header from "./Header/Header.jsx";
+
+export { Footer, Header };
