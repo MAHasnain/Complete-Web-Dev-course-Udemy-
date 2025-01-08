cf87cff243c96ce724fb3049b08d02fb2c5e45e0
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-01-08 17:53:24 +0500

    Implement routing with React Router; add Home, Login, AllPosts, AddPost, EditPost, and Post components with authentication layout

--- Frontend_section/09_Mega-Project_Blog-App/src/main.jsx
+++ Frontend_section/09_Mega-Project_Blog-App/src/main.jsx
@@ -4,11 +4,74 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Home,
+  AddPost,
+  AllPosts,
+  EditPost,
+  Login,
+  Post,
+  SignUp,
+} from "./pages";
+import { Protected as AuthLayout } from "./components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
